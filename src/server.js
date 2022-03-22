61b3879c3f6a85a822bc33ff88aab11c80becbbe
riggage <riggage@hotmail.co.uk>
2022-03-22 14:47:38 +0000

    put things into server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,16 @@
 require("./db/connection");
 const express = require("express");
 const cors = require("cors");
+const albumRouter = require("./albums/albumRoutes");
+const userRouter = require("./user/userRoutes");
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use(albumRouter);
+app.use(userRouter);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
