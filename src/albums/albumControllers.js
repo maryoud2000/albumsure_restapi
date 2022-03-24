1d9aee775e11ca351147e0a26dd62809fc0dd7ff
riggage <riggage@hotmail.co.uk>
2022-03-24 16:13:36 +0000

    finished the list and delete

--- src/albums/albumControllers.js
+++ src/albums/albumControllers.js
@@ -9,3 +9,29 @@ exports.addAlbum = async (req, res) => {
       res.status(500).send({ err: error.message });
     }
   };
+
+  exports.listAlbum = async (req, res) => {
+    try {
+        const album = await Album.find({});
+        res.status(200).send({ allAlbum: album })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({err: error.message})
+    }
+}
+
+exports.deleteAlbum = async (req, res) => {
+  try {
+      const deleteAlbum = await Album.deleteOne({
+          [req.params.filterKey]: req.params.filterVal,
+      });
+      if (deleteAlbum.deletedCount >0) {
+          res.status(200).send({ msg: "Successfully deleted album from list" });
+      } else {
+          throw new Error("Did not delete");
+      }
+  } catch (error) {
+      console.log(error);
+      res.status(500).send({ err: error.message });
+  }
+}
